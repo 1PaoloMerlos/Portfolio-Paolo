1aa5adb3689472a8561212aecd658a651b758c31
Paolo Merlos <merlospaolo@gmail.com>
2023-06-02 11:33:05 +0300

    major changes, scrolling projects and respons

--- script.js
+++ script.js
@@ -19,4 +19,75 @@ $(document).ready(function() {
       $('.projects.' + value).show('3000');
     }
   });
+
+  $('.projects').click(function() {
+    $(this).find('.project-details').slideToggle();
+  });
+
+  const projectsContainer = document.getElementById('projects-container');
+  const scrollTrigger = document.getElementById('scroll-trigger');
+
+  // Function to add a new project to the container
+  function addProject() {
+    const projectHTML = `
+      <!-- ...existing project HTML... -->
+    `;
+    const projectElement = document.createElement('div');
+    projectElement.classList.add('projects');
+    projectElement.innerHTML = projectHTML;
+    projectsContainer.appendChild(projectElement);
+  }
+
+  // Add initial projects (you can replace this with your own logic to load projects)
+  for (let i = 0; i < 8; i++) {
+    addProject();
+  }
+
+  // Infinite scrolling
+  projectsContainer.addEventListener('scroll', function() {
+    if (projectsContainer.scrollTop + projectsContainer.clientHeight >= scrollTrigger.offsetTop) {
+      addProject();
+    }
+  });
+
+  // Load initial projects
+  loadProjects();
+
+  // Infinite scroll
+  $('#projects-container').scroll(function() {
+    if ($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+      loadProjects();
+    }
+  });
+
+  // Function to load projects
+  function loadProjects() {
+    var projects = [
+      {
+        name: 'Project 1',
+        description: 'Description of Project 1',
+        imageSrc: '/Content/project1.jpg'
+      },
+      // Add more projects as needed
+    ];
+
+    var projectRow = $('#project-row');
+
+    // Append projects to the project row
+    projects.forEach(function(project) {
+      var projectHTML = `
+        <div class="projects">
+          <div class="project-thumbnail">
+            <img src="${project.imageSrc}" alt="${project.name}">
+          </div>
+          <h3>${project.name}</h3>
+          <div class="project-details">
+            <p>${project.description}</p>
+          </div>
+        </div>
+      `;
+
+      projectRow.append(projectHTML);
+    });
+  }
 });
