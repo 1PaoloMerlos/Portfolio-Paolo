dd66b6ef48ba8bd81ac6b9c557442fcc2c3274bb
Paolo Merlos <merlospaolo@gmail.com>
2025-04-12 00:53:08 +0300

    Design Update

--- Content/scripts/script.js
+++ Content/scripts/script.js
@@ -6,37 +6,15 @@ $(document).ready(function() {
             const projectId = `${project.id}`;
             $('.projects').append(`
                 <div class="project" id="${projectId}"> 
+                 <div class="bg-gray-800 p-6 rounded-lg">
+                    <img src="${project.image}" alt="${project.name}" class="w-full h-48 object-cover rounded-md">
                     <h3>${project.name}</h3>
                     <p class="project-description">${project.description}</p>
                     <p class="technologies">${project.technologies}</p>
-                    <button class="show-more" data-project-id="${projectId}"> More Info </button>
-                </div>
-            `);
-            $('.content-projects').append(`
-                <div class="project-content hidden md:max-w-2xl p-5" id="section-${projectId}">
-                    <p class="project-description">${project.description}</p>
-                    <p class="technologies pt-2">${project.technologies}</p>
-                    <button><a href="${project.url}" target="_blank">View Project</a><button>
-                </div>
+                    <button><a href="${project.url}" target="_blank">View Project</a></button>
+                </div></div>
             `);
         });
     });
 
-    //on click show more button, show the content of each individual project of their corresponding id
-$('.projects').on('click', '.show-more', function() {
-    const projectId = $(this).data('project-id');
-    const projectContent = $(`#section-${projectId}`);
-        
-    //loop through project content and show the content of the project that matches the id of the button clicked
-    $.each($('.project-content'), function(index, content) {
-        if ($(this).attr('id') !== `section-${projectId}`) {
-            $(this).addClass('hidden');
-            }
-
-        else {
-            projectContent.removeClass('hidden');
-            projectContent.addClass('showing');
-            }
-        });
-    });
 });
